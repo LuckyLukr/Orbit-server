e206498aedae38ddabd0c0cbe1b3427bf302f408
Luke <mrlukr11@gmail.com>
2021-02-22 22:26:38 +0100

    fix: refactor hard coded connection string to Heroku config var

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb+srv://Lukr:Langeer425401477@taskappcluster.96ojv.mongodb.net/ORBIT_DB?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
